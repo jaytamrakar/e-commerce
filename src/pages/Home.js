c4e7220d3f85fc803cfa8c19ac29759e47954a2b
jaytamrakar <jaytamrakar5@gmail.com>
2023-06-20 02:33:07 +0530

    Product List Template ready

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from '../features/navbar/NavBar'
+import ProductList from '../features/product-list/ProductList'
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar>
+        <ProductList>
+            
+        </ProductList>
+      </NavBar>
+    </div>
+  )
+}
+
+export default Home
